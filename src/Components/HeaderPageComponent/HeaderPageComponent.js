875862d617986a985d347e4bf2c70f795fc50841
Shikhar15606 <shikharshukla15606@gmail.com>
2020-09-30 14:53:46 +0530

    Signup/Login Logout with ui change completed

--- src/Components/HeaderPageComponent/HeaderPageComponent.js
+++ src/Components/HeaderPageComponent/HeaderPageComponent.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import {
-    Link
-} from 'react-router-dom';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 import firebase from 'firebase';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+  } from 'reactstrap';
 
 function HeaderPageComponent(props) {
+    const [name , setname ] = useState(null);
+    const [email , setemail ] = useState(null);
+    const [profilepic , setprofilepic ] = useState(null);
+    const [isloggedin , setisloggedin ] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          setisloggedin(true);
+          setemail(user.email);
+          setname(user.displayName);
+          setprofilepic(user.photoURL);
+        } else {
+          setisloggedin(false);
+          setisloggedin(false);
+          setprofilepic(null);
+          setname(null);
+          setemail(null);
+        }
+      });
+
     var provider = new firebase.auth.GoogleAuthProvider();
     var db = firebase.firestore();
     var login = (event) => {
@@ -20,10 +44,7 @@ function HeaderPageComponent(props) {
                 .then(function(querySnapshot) {
                     console.log("Then Block", querySnapshot);
                     if (querySnapshot.docs.length !== 0) {
-                        querySnapshot.forEach((doc) => {
-                            console.log(doc.exists);
                             console.log("Already Registered !");
-                        })
                     } else {
                         console.log("Not Already Registered !!!");
                         db.collection("users").add({
@@ -41,6 +62,7 @@ function HeaderPageComponent(props) {
                             });
                         console.log(user);
                     }
+                    console.log("Logged in");
                 })
                 .catch(function(error) {
                     console.log("Error getting documents: ", error);
@@ -56,18 +78,49 @@ function HeaderPageComponent(props) {
             // ...
         });
     }
+
+    var logout = (event)=>{
+        event.preventDefault();
+        firebase.auth().signOut().then(function() {
+            console.log("Logged Out");
+          }).catch(function(error) {
+            console.log("Some error in logging out");
+          });
+    }
     return ( 
     <div>
-        <nav className = "navbar navbar-expand-lg navbar-light bg-light" >
-        <Link className = "navbar-brand" to = "/home" > Navbar </Link> 
-        <div className = "collapse navbar-collapse" id = "navbarSupportedContent">
-            <ul className = "navbar-nav ml-auto" >
-                <li className = "nav-item" >
-                    <button className = "btn btn-outline-success my-2 my-sm-0" type = "submit" onClick = {login}>Login</button> 
-                </li> 
-            </ul> 
-        </div> 
-        </nav>
+      <Navbar color="light" light expand="md">
+      <Link className = "navbar-brand" to = "/home" > V Chat </Link> 
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+        
+                {isloggedin ?
+                <React.Fragment>
+                    <ul className = "navbar-nav ml-auto" >
+                        <li className ="nav-item">
+                            <img src= {profilepic} alt={profilepic} style={{maxWidth:"55px",maxHeight:"55px"}} class="rounded-circle" />
+                        </li> 
+                    </ul>
+                    <ul className = "navbar-nav" >
+                        <li className ="nav-item">
+                            <h3 style={{padding:"10px"}}>{name}</h3>
+                        </li> 
+                    </ul>
+                    <ul className = "navbar-nav ml-auto" >
+                        <li className = "nav-item ml-auto" >
+                            <button className = "btn btn-outline-success my-2 my-sm-0" type = "submit" onClick = {logout}>Logout</button> 
+                        </li> 
+                    </ul>
+                </React.Fragment>
+                :
+                <ul className = "navbar-nav ml-auto" >
+                    <li className = "nav-item" >
+                        <button className = "btn btn-outline-success my-2 my-sm-0" type = "submit" onClick = {login}>Login</button> 
+                    </li>
+                </ul>
+                }  
+        </Collapse>
+      </Navbar>
     </div>
     );
 }
