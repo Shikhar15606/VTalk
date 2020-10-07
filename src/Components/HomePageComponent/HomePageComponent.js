daa0a2eb2392ad7143cd1b7480870d2a2bb83b7f
Shikhar15606 <shikharshukla15606@gmail.com>
2020-10-08 01:42:17 +0530

    Version 2

--- src/Components/HomePageComponent/HomePageComponent.js
+++ src/Components/HomePageComponent/HomePageComponent.js
@@ -7,9 +7,13 @@ function HomePageComponent({isloggedin,email}) {
     let unsubscribe = ()=>{
         console.log("No need For cleanup now");
     };
+    let unsubscribe2 = ()=>{
+        console.log("No need For cleanup now");
+    };
     const [isloading,setisloading] = useState(true);
     var db = firebase.firestore();
     const [list,setlist] = useState([]);
+    const [arr,setarr] = useState([]);
     // simple array becomes undefined in return 
 
     useEffect(()=>{
@@ -23,15 +27,29 @@ function HomePageComponent({isloggedin,email}) {
                 })
                 console.log("data",data);
                 setlist(data);
-                setisloading(false);
+                // setisloading(false);
         });
     } 
     },[])
 
-    
+    useEffect(() =>{
+        console.log(isloading);
+        unsubscribe2 = db.collection("messages").where("receiver", "==", email).where("isview","==",false)
+        .onSnapshot(function(querySnapshot) {
+            let data2 = [];
+                querySnapshot.forEach(function(doc) {
+                    data2.push(doc.data().sender);
+                })
+                console.log("data2",data2);
+                setarr(data2);
+                setisloading(false);
+        });
+    },[])
+
     useEffect(() => { 
         return function cleanup () {
           unsubscribe();
+          unsubscribe2();
           // if you return a function from useeffect then it will be executed after component unmounts
         }
      }, [])
@@ -42,7 +60,7 @@ function HomePageComponent({isloggedin,email}) {
         <React.Fragment>
             {
             !isloading ?
-            <div style={{marginTop:"80px"}}>
+            <div className="top-margin">
                 {    
                     list.map(item => (
                         (
@@ -54,21 +72,28 @@ function HomePageComponent({isloggedin,email}) {
                                     {
                                         item.isonline ?
                                         <div style={{zIndex:1,borderRadius:"50%",top:"35px",left:"50px",width:"21px",height:"21px",backgroundColor:"white",position:"absolute"}}>
-                                            <div style={{zIndex:2,borderRadius:"50%",top:"4px",left:"4px",width:"11px",height:"11px",backgroundColor:"rgb(2 161 253)",position:"absolute"}}>
+                                            <div style={{zIndex:2,borderRadius:"50%",top:"4px",left:"4px",width:"11px",height:"11px",backgroundColor:"#5BC236",position:"absolute"}}>
                                             </div>
                                         </div>
                                         :
                                         <span></span>
                                     }
                                 </div>
-                                <div className="col-7 col-md-8 offset-1 offset-md-1" >
+                                <div className="col-6 col-md-6 offset-1 offset-md-1" >
                                     <div className="row">
-                                        <h5 style={{textAlign:"left"}} >{item.name}</h5>
+                                        <h6 style={{textAlign:"left"}} >{item.name}</h6>
                                     </div>
                                     <div className="row">
                                         <h7>{item.email}</h7>
                                     </div>
                                 </div>
+                                {
+                                        arr.includes(item.email) ?
+                                            <div className="newmsgdot">
+                                            </div>
+                                        :
+                                        <span></span>
+                                }
                                 </div>
                                 <hr />
                                 </Link>
